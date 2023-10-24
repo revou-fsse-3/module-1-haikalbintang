2efa2c664f8a6f3493c63b15cc59b7b7b6572662
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2023-10-24 11:34:33 +0700

    add bg img; kodingan masih berantakan

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+"use strict";
+
 const likeButton = document.getElementById("increment-button");
+const loveButton = document.getElementById("love-test");
 const logoNotif = document.getElementById("logo-notif");
 
 let notif = 0;
+// let love = 0;
 
 function totalNotif() {
   notif++;
@@ -9,6 +13,13 @@ function totalNotif() {
   if (notif > 9) {
     alert("Welcome to the Club, Cheetah lovers !! 😊😊");
   }
+  console.log(notif);
 }
 
+// function lovedImage() {}
+
+// Like Button //
 likeButton.onclick = totalNotif;
+
+// Love Button //
+loveButton.onclick = totalNotif;
